93c1b1f2972af813bc4bdec4b1688b94dffef4a9
Peter <peter.balogh.wien@gmail.com>
2025-07-14 11:03:49 +0200

    Finished 2D-section script

--- data_processing/Bruenlital/90_scripts/2d_section/A1_section-IP_blk-04lay_v00.py
+++ data_processing/Bruenlital/90_scripts/2d_section/A1_section-IP_blk-04lay_v00.py
@@ -3,35 +3,41 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from typing import Union
+import matplotlib
+import matplotlib.cm
+import matplotlib.figure
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolor
 
 #%% Inputs
 
 # Name of the 2D section
-profile_name  = '20250710_A1'
+profile_name: str  = '20250710_A1'
 
 # Inversion type
-inversion_type = 'pyGIMLi'
+inversion_type: str = 'pyGIMLi'
 
 # Version of the 2D section
-section_version = 'v00'
+profile_version: str = 'v00'
 
 # Coordinates (relative paths from .../30_inv-results/{inversion_type}/)
-coordinate_file = '50_coord/20250710_coords_centers.csv'
+coordinate_file: str = '50_coord/20250710_coords_centers.csv'
 
 # Column names of DataFrame
-name_column = 'Name'
-easting_column = 'Easting'
-northing_column = 'Northing'
-elevation_column = 'Elevation'
+name_column: str = 'Name'
+easting_column: str = 'Easting'
+northing_column: str = 'Northing'
+elevation_column: Union[str, bool] = 'Elevation'
 
 # Consider elevation
 # elevation_column = False
 
-#
+# How thick the last layer should be plotted
+add_bottom: Union[int, float] = 3 # in (m)
 
 # Chosen soundings (relative paths from .../30_inv-results/{inversion_type}/)
-sounding_files = [
+sounding_files: list = [
     'A1-IP_blk-04lay/20250710_A1/v00_tr5-100us/B001/csv/invrun000_B001.csv',
     'A1-IP_blk-04lay/20250710_A1/v00_tr5-100us/B002/csv/invrun000_B002.csv',
     'A1-IP_blk-04lay/20250710_A1/v00_tr5-100us/B003/csv/invrun000_B003.csv',
@@ -41,8 +47,8 @@ sounding_files = [
 ]
 
 # Index of the first and last sounding of the line
-start_index = 0
-end_index = -1
+start_index: int = 0
+end_index: int = -1
 
 #%% Paths
 
@@ -118,7 +124,7 @@ sounding_coordinates.sort_values(by='Distance', inplace=True)
 
 #%% Read Data and create a Dataframe with all results
 # Needed columns ("Name" will be added)
-inv_columns = ['Name', 'depth(m)', 'rho(Ohmm)', 'mpa(rad)', 'tau_p(s)', 'c()']
+inv_columns = ['Name', 'layer_number', 'depth(m)', 'layer_thickness', 'rho(Ohmm)', 'mpa(rad)', 'tau_p(s)', 'c()']
 
 # Reading all the results
 inversion_data = [pd.read_csv(data_path) for data_path in sounding_paths]
@@ -127,6 +133,11 @@ inversion_data = [pd.read_csv(data_path) for data_path in sounding_paths]
 inversion_df = pd.DataFrame()
 for name, df in zip(sounding_names, inversion_data):
     df['Name'] = name
+    depths = df['depth(m)'].copy()
+    depths[depths == 0] = add_bottom
+    df['layer_thickness'] = depths
+    df['depth(m)']= np.cumsum(depths)
+    df['layer_number'] = np.arange(len(df))
     inversion_df = pd.concat([inversion_df, df[inv_columns]])
 
 # Reset index
@@ -135,51 +146,95 @@ inversion_df.reset_index(drop=True, inplace=True)
 # Merging with the coordinates
 subsurface_model =  inversion_df.merge(sounding_coordinates, how='left', on='Name')
 
-#%% Create plots
+#%% Plotting function
 
-fig, ax = plt.subplots(2,2, figsize=(16, 8))
-ax = ax.ravel()
-sc = ax[0].scatter(
-    subsurface_model['Distance'], subsurface_model['depth(m)'], 
-    c=subsurface_model['rho(Ohmm)'], cmap='viridis', s=100, 
-    label='Resistivity', marker='s', 
-    vmin=subsurface_model['rho(Ohmm)'].min(), vmax=subsurface_model['rho(Ohmm)'].max())
-cbar = fig.colorbar(sc, ax=ax[0])
-ax[0].invert_yaxis()
+def plot_2d_section(elevation: Union[str, bool], mode: str = 'scatter') -> matplotlib.figure.Figure:
+    fig, ax = plt.subplots(2,2, figsize=(16, 8))
+    ax = ax.ravel()
 
-def inversion_plot_2D_section(lam=183, lay_thk={0:1, 5:1.5, 15:2}):
+    mode_list = ['scatter', 'bar']
 
+    model_df = subsurface_model.copy()
 
-    fig, ax = plt.subplots(figsize=(10, 6))
-    # Plotting the data
-    # Customize colorbar to match the data values
-    model_unit_list, dist_list, thks_list = [], [], []
-    thks = [1, 2, 3, 4, 5, 6.5, 8.0, 9.5, 11.0]
-    for sounding, dist in zip(range(11), distances):
-        model_unit = resistivities[sounding]
-        dist = [dist for _ in range(len(thks))]
-        for i, j, k in zip(model_unit, dist, thks):
-            model_unit_list.append(i)
-            dist_list.append(j)
-            thks_list.append(k)
+    if 'model_depth' not in model_df.columns:
+        if elevation:
+            model_df['model_depth'] = model_df[elevation] - model_df['depth(m)']
+        else:
+            model_df['model_depth'] = model_df['depth(m)']
 
-        
-    sc = ax.scatter(dist_list,thks_list, c=model_unit_list, cmap='viridis', s=150, label='pyGIMLI')
-    
-    cbar = fig.colorbar(sc, ax=ax)
-    # cbar.set_ticks(np.linspace(np.min(unit_values), np.max(unit_values), num=6),
-    #                 fontsize=14)  # Set ticks according to data range
-    cbar.set_label(r'$\rho$ ($\Omega$m)', fontsize=16)  # Set colorbar label
-    ax.set_xlabel("Horizontal Distance (m)")
-    ax.set_ylabel("Depth (m)")
-    ax.set_title(f"2D Crosssection with Multiple Soundings\n$\lambda$ = 183", fontsize=22, fontweight='bold')
-    ax.set_ylim(-2, 12)
-    for x_val, y_val, label in zip(distances, [0 for _ in range(len(distances))], intersections['Name']):
-        ax.text(x_val, y_val, label, fontsize=10, color="black", ha="center", va="bottom", rotation=45)
-   
-    ax.invert_yaxis()  # Invert y-axis to show depth increasing downward
-    fig.savefig('inversion_2Dsection_scatter_{}.png'.format('rhoa'))
+    model_parameters = ['rho(Ohmm)', 'mpa(rad)', 'tau_p(s)', 'c()']
+
+    first_layer_df = model_df[model_df['layer_number'] == 0].copy()
+    first_layer_df.reset_index(drop=True, inplace=True)
+
+    for i, parameter in enumerate(model_parameters):
+
+        y_lower_lim = max(model_df['model_depth'].min() * 0.9, model_df['model_depth'].min() - 10)
+        y_upper_lim = min(model_df['model_depth'].max() * 1.1, model_df['model_depth'].max() + 10)
 
 
-# %%
-# inversion_plot_2D_section()
+        if mode == 'scatter':
+            sc = ax[i].scatter(
+                model_df['Distance'], model_df['model_depth'], 
+                c=model_df[parameter], cmap='viridis', s=100, 
+                label='Resistivity', marker='s', 
+                vmin=model_df[parameter].min(), vmax=model_df[parameter].max())
+            _ = fig.colorbar(sc, ax=ax[i], label=parameter[parameter.find('('):])
+
+        elif mode == 'bar':
+            if elevation:
+                bar_bottom = first_layer_df[elevation].copy().values
+                ax[i].plot(first_layer_df['Distance'], bar_bottom, c='k', zorder=0, linewidth=0.8)
+            else:
+                bar_bottom = np.zeros_like(np.arange(len(sounding_files)), dtype='float64')
+
+            norm = mcolor.Normalize(vmin=model_df[parameter].min(), vmax=model_df[parameter].max())
+            cmap = matplotlib.colormaps['viridis']
+
+            for _, layer_df in model_df.groupby('layer_number'):
+                if elevation:
+                    bar_bottom -= layer_df['layer_thickness'].values
+                color = cmap(norm(layer_df[parameter].values))
+                ax[i].bar(layer_df['Distance'], layer_df['layer_thickness'], 2, bottom=bar_bottom, color=color)
+                if not elevation:
+                    bar_bottom += layer_df['layer_thickness'].values
+            
+            _ = fig.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax[i], label=parameter[parameter.find('('):])
+        else:
+            raise ValueError(f'"{mode}" is an invalid input. Must be from {mode_list}')
+
+        for j in range(len(first_layer_df)):
+            name = first_layer_df['Name'][j]
+            x_val = first_layer_df['Distance'][j]
+            y_val = y_upper_lim if elevation else 0
+            ax[i].text(
+                x_val, y_val, name, 
+                fontsize=10, color="black", 
+                ha='center', va="bottom")
+
+        ax[i].set_xlabel("Horizontal Distance (m)")
+        ax[i].set_ylabel("Depth (m)")
+        ax[i].set_ylim((y_lower_lim, y_upper_lim))
+        ax[i].set_title(parameter[:parameter.find('(')], fontweight='bold', fontsize=16, pad = 15.0)
+        if not elevation:
+            ax[i].set_ylim((0, y_upper_lim))
+            ax[i].invert_yaxis()
+
+    fig.suptitle(f'2D-section of the profile "{profile_name}"', fontweight='bold', fontsize=20)
+    fig.tight_layout()
+
+    return fig
+
+# %% Create plots
+
+fig1 = plot_2d_section(elevation=elevation_column, mode='scatter')
+fig1.savefig(result_path / f'{profile_name}_{profile_version}_scatter_elevation.png', dpi=300)
+
+fig2 = plot_2d_section(elevation=False, mode='scatter')
+fig2.savefig(result_path / f'{profile_name}_{profile_version}_scatter.png', dpi=300)
+
+fig3 = plot_2d_section(elevation=elevation_column, mode='bar')
+fig3.savefig(result_path / f'{profile_name}_{profile_version}_bar_elevation.png', dpi=300)
+
+fig4 = plot_2d_section(elevation=False, mode='bar')
+fig4.savefig(result_path / f'{profile_name}_{profile_version}_bar.png', dpi=300)
--- data_processing/Bruenlital/90_scripts/2d_section/readme_section_04lay.txt
+++ data_processing/Bruenlital/90_scripts/2d_section/readme_section_04lay.txt
@@ -1,7 +1,13 @@
 2D sections with the inv results of the 4 layer-model (readme_inv_04lay.txt)
 
 A1_section:
-    Soundings B001, B002, B003, B004, B005, B006
+    Name: 20250710_A1
+    Soundings: B001, B002, B003, B004, B005, B006
 
     v00:
-        
+        filter: v00_tr5-100us file: invrun000_B001.csv
+        filter: v00_tr5-100us file: invrun000_B002.csv
+        filter: v00_tr5-100us file: invrun000_B003.csv
+        filter: v00_tr5-100us file: invrun000_B004.csv
+        filter: v00_tr5-100us file: invrun000_B005.csv
+        filter: v00_tr5-100us file: invrun000_B006.csv
